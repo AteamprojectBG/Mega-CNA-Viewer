c720f4caa25c2ff1601093d9b2ffe1c1a2683708
Dmitry Utkin <dutking@yandex.ru>
2024-05-02 11:23:13 +0400

    documented and linted

--- src/cna_table.js
+++ src/cna_table.js
@@ -1,12 +1,16 @@
+/**
+ * @file Class representing a table of DR and BAF theoretical distribution based on given purity, ploidy and copy numbers.
+ */
+
 /** Class representing a table of DR and BAF theoretical distribution. */
 class CNATable {
     /**
      * Create a table of DR and BAF theoretical distribution.
-     * @param {number} purity - Fraction of tumor DNA in the sample
-     * @param {number} ploidy - Number of sets of chromosomes
-     * @param {Array} copies - List of numbers of copies
-     * @param {number} normal_ploidy - Number of sets of chromosomes in the normal sample
-     * @return {Object} Object with lists of BAF, DR, total and minor
+     * @param {number} purity - Fraction of tumor DNA in the sample.
+     * @param {number} ploidy - Number of sets of chromosomes.
+     * @param {Array} copies - List of numbers of copies.
+     * @param {number} normal_ploidy - Number of sets of chromosomes in the normal sample.
+     * @returns {object} Object with lists of BAF, DR, total and minor.
      */
     constructor(purity=1, ploidy=2, copies=[2,3,4], normal_ploidy = 2) {
         this.purity = purity;
@@ -18,11 +22,11 @@ class CNATable {
 
     /**
      * Returns the frequency of B allele based on number of B alleles, purity and total number of copies of the region.
-     * @param {number} minor - Number of B alleles
-     * @param {number} purity - Fraction of tumor DNA in the sample
-     * @param {number} total - Total number of copies of the region
-     * @param {number} normal_ploidy - Number of sets of chromosomes in the normal sample
-     * @return {number} Frequency of B allele
+     * @param {number} minor - Number of B alleles.
+     * @param {number} purity - Fraction of tumor DNA in the sample.
+     * @param {number} total - Total number of copies of the region.
+     * @param {number} normal_ploidy - Number of sets of chromosomes in the normal sample.
+     * @returns {number} Frequency of B allele.
      */
     static #getBAlleleFrequency(minor, purity, total, normal_ploidy){
         const baf = ((minor * purity) + (1 - purity)) / (
@@ -33,11 +37,11 @@ class CNATable {
 
     /**
      * Returns the ratio of tumor coverage to normal coverage based on purity, ploidy and total number of copies of the region.
-     * @param {number} purity - Fraction of tumor DNA in the sample
-     * @param {number} ploidy - Number of sets of chromosomes
-     * @param {number} total - Total number of copies of the region
-     * @param {number} normal_ploidy - Number of sets of chromosomes in the normal sample
-     * @return {number} Ratio of tumor coverage to normal coverage
+     * @param {number} purity - Fraction of tumor DNA in the sample.
+     * @param {number} ploidy - Number of sets of chromosomes.
+     * @param {number} total - Total number of copies of the region.
+     * @param {number} normal_ploidy - Number of sets of chromosomes in the normal sample.
+     * @returns {number} Ratio of tumor coverage to normal coverage.
      */
     static #getDepthRatio(purity, ploidy, total, normal_ploidy){
         const depth_ratio = ((1 - purity) + ((total / normal_ploidy) * purity)) / (
@@ -48,8 +52,8 @@ class CNATable {
 
     /**
      * Returns the max value for a range of possible b numbers.
-     * @param {number} total - Total number of copies of the region
-     * @return {number} Max value for a range of possible b numbers
+     * @param {number} total - Total number of copies of the region.
+     * @returns {number} Max value for a range of possible b numbers.
      */
 
     static #getMaxBNum(total){
@@ -58,10 +62,11 @@ class CNATable {
 
     /**
      * Returns a dictionary with lists of BAF, DR, total and minor.
-     * @param {number} purity - Fraction of tumor DNA in the sample
-     * @param {number} ploidy - Number of sets of chromosomes
-     * @param {number} copy_numbers - List of numbers of copies
-     * @return {Object} Object with lists of BAF, DR, total and minor
+     * @param {number} purity - Fraction of tumor DNA in the sample.
+     * @param {number} ploidy - Number of sets of chromosomes.
+     * @param {number} copy_numbers - List of numbers of copies.
+     * @param {number} normal_ploidy - Number of sets of chromosomes in the normal sample.
+     * @returns {object} Object with lists of BAF, DR, total and minor.
      */
     static #buildCNTable(purity, ploidy, copy_numbers, normal_ploidy = 2){
         const table = []
